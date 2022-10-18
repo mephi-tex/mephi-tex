f8d53575d20d9e151d9bc3d5c493ec66fb69bcd2
0dminnimda <0dminnimda@gmail.com>
2022-10-18 07:39:46 +0300

    setup.py: fix the import

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@
 Fixes all of the files before they can be rendered
 """
 
-from dosc.preprocessing import preprocess_for_server
+from docs.preprocessing import preprocess_for_server
 
 preprocess_for_server()
