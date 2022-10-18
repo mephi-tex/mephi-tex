a09943c3ce13fd053393bd81a35f64f52a6d5615
0dminnimda <0dminnimda@gmail.com>
2022-10-18 07:28:03 +0300

    conf.py: update according to preprocessing.py, use relative imports

--- docs/conf.py
+++ docs/conf.py
@@ -6,20 +6,22 @@
 
 from pathlib import Path
 
+# -- Preprocess files --------------------------------------------------------
+
+from .preprocessing import preprocess_locally
+
+preprocess_locally()
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
-import os
-import sys
+# import os
+# import sys
 
-sys.path.insert(0, os.path.abspath("."))
-
-from preprocessing import run_all  # noqa
-
-run_all(["IVT", "IVT_evening"])
+# sys.path.insert(0, os.path.abspath("."))
 
 # -- Project information -----------------------------------------------------
 
